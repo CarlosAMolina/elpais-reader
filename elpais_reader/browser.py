c04979f75aac7bceff8edb07503ca4a2be41679f
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-05-29 14:27:37 +0200

    Add webbrowser module

---
+++ elpais_reader/browser.py
@@ -0,0 +1,16 @@
+import export
+import webbrowser
+import logging
+
+
+class Browser:
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+
+    def open_html(self) -> None:
+        self._logger.debug(f"Init: {self._url}")
+        webbrowser.open_new_tab(self._url)
+
+    @property
+    def _url(self) -> str:
+        return f"file:///{export.HtmlExporter().filepath}"
